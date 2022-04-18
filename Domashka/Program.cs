0378ed26e14493f30c271b5fb526d9c1f6249406
Lev <levasik228@mail.ru>
2022-04-18 21:34:54 +0500

    Zadacha a + 2b + 3c

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("H0101010");
+double a;
+double b;
+double c;
+double d;
+string itog;
+a = 5.0;
+b = 2.0;
+c = 3.0;
+d = a + 2*b + 3*c;
+itog = a + " + 2*" + b + " + 3*" +  c + " = " + d;
+Console.WriteLine(itog);
+// 5 + 2*2 + 3*3 = 18
+// x     x     x   xx     
+// 5 + 2*4 + 3*9 = 18
