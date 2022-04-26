f10247f06402c32cb626529a90d92cb28ceae864
Lev <levasik228@mail.ru>
2022-04-26 21:29:50 +0500

    user can enter numbers from the keyboard

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,15 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double a;
-double b;
-double c;
+Console.Write("a=");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b=");
+int b = int.Parse(Console.ReadLine());
+Console.Write("c=");
+int c = int.Parse(Console.ReadLine());
 double d;
 string itog;
-a = 5.0;
-b = 2.0;
-c = 3.0;
 d = a + 2*b + 3*c;
 itog = a + " + 2*" + b + " + 3*" +  c + " = " + d;
 Console.WriteLine(itog);
-// 5 + 2*2 + 3*3 = 18
-// x     x     x   xx     
-// 5 + 2*4 + 3*9 = 18
