207a6084cb2957fac05f9d46ff534bd198a677aa
Lev <levasik228@mail.ru>
2022-04-28 09:34:12 +0500

    the user enters into the square, you need to output the square

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,12 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("a=");
-int a = int.Parse(Console.ReadLine());
-Console.Write("b=");
-int b = int.Parse(Console.ReadLine());
-Console.Write("c=");
-int c = int.Parse(Console.ReadLine());
-double d;
-string itog;
-d = a + 2*b + 3*c;
-itog = a + " + 2*" + b + " + 3*" +  c + " = " + d;
-Console.WriteLine(itog);
+﻿//пользователь вводит сторону квадрата, нужно вывести площадь квадрата
+// S = a × a, где S — площадь, a — сторона.
+Console.Write("Введите сторону квадрата(a):");
+double a = double.Parse(Console.ReadLine());
+double s = a * a;
+Console.Write("s= " + a + "*" + a);
+Console.WriteLine("; S=" + s);
+Console.ReadKey();
+
+//Console.Readkey решил по приколу написать
+
+
+
