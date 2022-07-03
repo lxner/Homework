7b4416a1013b49b2977148017293179c8c3081eb
Lev <levasik228@mail.ru>
2022-07-03 19:26:53 +0500

    T_1_25

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_1_22_B prog = new T_1_22_B();
+T_1_25 prog = new T_1_25();
 prog.Go();
---
+++ Domashka/T_1_25.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_1_25
+    {
+        public void Go()
+        {
+            Console.Write("Введите сторону квадрата: ");
+            double a = double.Parse(Console.ReadLine());
+            double p = 4*a;
+            Console.Write("Периметр квадрата = " + p);
+
+        }
+    }
+}
+
+
+// Дана сторона квадрата. Найти его периметр. P=4*a.
