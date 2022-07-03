954a81efbaded01b7912a99ebb8ff56567b775eb
Lev <levasik228@mail.ru>
2022-07-03 19:36:35 +0500

    T_1_26

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_1_25 prog = new T_1_25();
+T_1_26 prog = new T_1_26();
 prog.Go();
---
+++ Domashka/T_1_26.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_1_26
+    {
+        public void Go()
+        {
+            Console.Write("Введите радиус: ");
+            double r = double.Parse(Console.ReadLine());
+            double d = 2*r;
+            Console.WriteLine("Диаметр равен = " + d);
+        }
+    }
+}
+
+
+
+// Дан радиус окружности, найти её диаметр. d = 2*r
