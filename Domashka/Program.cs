ce311843e5d46e044731652ef7125f0963360df3
Lev <levasik228@mail.ru>
2022-07-04 15:07:19 +0500

    T_1_29

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_1_28 prog = new T_1_28();
+T_1_29 prog = new T_1_29();
 prog.Go();
---
+++ Domashka/T_1_29.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_1_29
+    {
+        public void Go()
+        {
+            Console.Write("Введите радиус окружности: ");
+            double r = double.Parse(Console.ReadLine());
+            double d = 2*3.1415*r;
+            Console.WriteLine("Длина окружности равна: " + d);
+            double s = 3.1415*r*r;
+            Console.WriteLine("Плошадь круга равна: " + s);
+        }
+    }
+}
+
+
+
+// дан радиус окружности. Найти длину окружности и площадь круга.
