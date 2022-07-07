7186fc231155d2ff53b65e926bfeb2c615b8ad24
Lev <levasik228@mail.ru>
2022-07-07 15:09:24 +0500

    T_1_33

--- Domashka/T_1/Old/Program.cs
+++ Domashka/T_1/Old/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_1_32 prog = new T_1_32();
+T_1_33 prog = new T_1_33();
 prog.Go();
---
+++ Domashka/T_1/T_1_33.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_1_33
+    {
+        public void Go()
+        {
+            Console.WriteLine("Количество жителей: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Плошадь территории: ");
+            double b = double.Parse(Console.ReadLine());
+            double c = a/b;
+            Console.WriteLine("Плотность населения равна: " + c);
+        }
+    }
+}
+
+
+// Известно кол-во жителей в государстве и плошадь его территории. Определить плотность населения в этом гос-ве.
