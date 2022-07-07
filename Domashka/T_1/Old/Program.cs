71a40bf629a4f47009cc3e421f6ec5ddb8d5bd9b
Lev <levasik228@mail.ru>
2022-07-07 14:56:15 +0500

    T_1_32

--- Domashka/T_1/Old/Program.cs
+++ Domashka/T_1/Old/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_1_31 prog = new T_1_31();
+T_1_32 prog = new T_1_32();
 prog.Go();
---
+++ Domashka/T_1/T_1_32.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_1_32
+    {
+        public void Go()
+        {
+            Console.WriteLine("Объём: ");
+            double v = double.Parse(Console.ReadLine());
+            Console.WriteLine("масса тела: ");
+            double m = double.Parse(Console.ReadLine());
+            double a = m/v;
+            Console.WriteLine("Плотность материала этого тела равна: " + a);
+        }
+    }
+}
