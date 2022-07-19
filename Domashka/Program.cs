4e90a0e466338f4e64c14983a13b382d7a57e1ac
Lev <levasik228@mail.ru>
2022-07-20 00:56:17 +0500

    T_2_13

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_2_11 prog = new T_2_11();
+T_2_13 prog = new T_2_13();
 prog.Go();
---
+++ Domashka/T_2/T_2_13.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_2_13
+    {
+        public void Go()
+        {
+            Console.WriteLine("Введите число: ");
+            int a = int.Parse(Console.ReadLine());
+            int c = a%10; // единицы
+            int b = (a/10)%10; // десятки
+            int d = a/100; // сотни
+            Console.WriteLine("сотни " + d);
+            Console.WriteLine("десятки " + b);
+            Console.WriteLine("единицы " + c);
+            int w = (c*100) + (b*10) + d;
+            Console.WriteLine(w);
+
+        }
+    }
+}
+
+
+// Дано трёхзначное число. Найти число, полученное при прочтении его цифр справа налево 
