012d51f26ac193d3182ff80835cec39278cb0026
Lev <levasik228@mail.ru>
2022-07-21 00:51:00 +0500

    T_2_15

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_2_14 prog = new T_2_14();
+T_2_15 prog = new T_2_15();
 prog.Go();
---
+++ Domashka/T_2/T_2_15.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_2_15
+    {
+        public void Go()
+        {
+            Console.WriteLine("Введите число: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = a%10; // единицы
+            int c = (a/10)%10; // десятки
+            int d = a / 100; // сотни
+            int z = b*100 + d*10 + c;
+            Console.WriteLine("Ответ: " + z);
+        }
+    }
+}
