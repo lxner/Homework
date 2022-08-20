9a3d2c7a8efc361a4e8682ebf18bef1d45f61429
Lev <levasik228@mail.ru>
2022-08-21 00:31:17 +0500

    exclusive

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Domashka;
 
-T_5_29 prog = new T_5_29();
+AsumB prog = new AsumB();
 prog.Go();
---
+++ Domashka/exclusive/AsumB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class AsumB
+    {
+        public void Go()
+        {
+            int sum = 0;
+            Console.WriteLine("Введите число от а: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число до b: ");
+            int b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                sum = sum + i;
+                
+            }
+            Console.WriteLine("сумма чисел = " + sum);
+        }
+    }
+}
+
+// посчитать сумму от a до b
