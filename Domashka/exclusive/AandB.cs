96f16de8e494c73a14c81a649baa279c6329d1c8
Lev <levasik228@mail.ru>
2022-08-21 02:00:11 +0500

    AandB

--- Domashka/exclusive/AandB.cs
+++ Domashka/exclusive/AandB.cs
@@ -13,12 +13,15 @@ namespace Domashka
             Console.WriteLine("Введите число от а: ");
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите число до b: ");
-            int b = int.Parse(Console.ReadLine());            
-            for (int i = a; i <= b; i++)
+            int b = int.Parse(Console.ReadLine());
+            int proizv = -1;
+            for (int i = a; i <= b; i++)          
             {
+                proizv = proizv + 1;
                 
             }
-            Console.WriteLine("Кол-во чисел = " + b);
+           
+            Console.WriteLine("Кол-во чисел = " + proizv);
         }
     }
 }
