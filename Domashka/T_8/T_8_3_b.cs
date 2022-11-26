98b323c0a168b9f731f8024ee4f8742c5f8620fb
Lev <levasik228@mail.ru>
2022-11-27 01:33:21 +0500

    T_8_3_b

--- Domashka/T_8/T_8_3_b.cs
+++ Domashka/T_8/T_8_3_b.cs
@@ -8,5 +8,18 @@ namespace Domashka
 {
     internal class T_8_3_b
     {
+        // T_8_4_B
+        public void Go()
+        {
+            for (int i = 6; i >= 2; i--)
+            {
+                for (int j = 0; j < i-1; j++)
+                {
+                    Console.Write(i-j + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
     }
 }
