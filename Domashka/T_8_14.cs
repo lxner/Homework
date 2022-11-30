2517c5a353488f9f60315e7a761f7f811e3022c4
Lev <levasik228@mail.ru>
2022-12-01 01:27:26 +0500

    T_8_14

--- Domashka/T_8_14.cs
+++ Domashka/T_8_14.cs
@@ -14,80 +14,70 @@ namespace Domashka
         {
             
             Random random = new Random();
-            //for (int i = 1; i <= 15; i++)
-            //{
-            //    Console.WriteLine(i + " студент = ");
-            //    for (int j = 1; j <= 3; j++)
-            //    {
-            //        Console.WriteLine("оценка по " + j + " предмету = " + random.Next(1, 5));
-            //        if (j != 2)
-            //        {
-            //            dvoikic++;
-            //        }
-            //    }
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine("Кол-во студентов сдавших сессию без 2 = " + dvoikic);
 
             // if (a[i][0]!=2 && a[i][1]!=2 && a[i][2]!=2)
 
-            // заполняем массив
-            //int i = 15;
-            //int j = 3;
-            //int[,] array = new int[i, j];
-            //for (i = 0; i < array.GetLength(0); i++) //студенты
-            //{
-            //    Console.WriteLine(i + " Студент = ");   
-            //    for (j = 0; j < array.GetLength(1); j++) //оценки
-            //    {
-            //        array[i, j] = random.Next(1,5);
-            //        Console.WriteLine("оценка по " + array[i,j] + " предмету = " + array[i,j]);
-            //    }
-            //    Console.WriteLine();
-            //}
-            //// работа с имеющщимеся данными
-            //Console.WriteLine();
-            //for (i = 0; i < array.GetLength(0); i++)
-            //{
-
-            //        // проверяю каждую оценку студента на 2
-            //        if (array[i, 0] > 2 && array[i, 1] > 2 && array[i, 2] > 2)
-            //        {
-            //            count++;
-            //        }
-
-            //}
-            //Console.WriteLine("Кол-во студентов сдавших сессию без 2 = " + count);
-            int count = 0;
-            Console.WriteLine("Введите n: ");
-            int n = int.Parse(Console.ReadLine());
-            Random random2 = new Random();
-            int[] arr = new int[n];
-            Console.WriteLine();
-            for (int i = 0; i < arr.Length; i++)
+             // заполняем массив
+             int count = 0;
+            int i = 15;
+            int j = 3;
+            int[,] array = new int[i, j];
+            for (i = 0; i < array.GetLength(0); i++) //студенты
             {
-                arr[i] = random2.Next(1,5);
-                Console.WriteLine(arr[i]);
-            }
-            Console.WriteLine();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < 3 && arr[i] > 0)
+                Console.WriteLine(i + " Студент = ");
+                for (j = 0; j < array.GetLength(1); j++) //оценки
                 {
-                    count++;
+                    array[i, j] = random.Next(1, 5);
+                    Console.WriteLine("оценка по " + (j+1) + " предмету = " + array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            // работа с имеющщимеся данными
+            Console.WriteLine();
+            // int[] countt = new int[i];
+            // проверяю каждую оценку студента на 2
+            for (i = 0; i < array.GetLength(0); i++)   // student
+            {
+
+                for (j = 0; j < array.GetLength(1); j++)   // оценки
+                {
                     
+                    if (array[i,0] > 2 && array[i,1] > 2 && array[i,2] > 2)
+                    {
+                        count++;
+                    }
                 }
-               
             }
-            for (int i = 0; i < 1; i++)
-            {
-                if (count > 0)
-                {
-                    Console.WriteLine("кол-во плохих оценок: " +  count);
+            Console.WriteLine("кол-во студентов сдавших без 2: " + count/3);
 
-                }
-            }
-            
+            //int count = 0;
+            //Console.WriteLine("Введите n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //Random random2 = new Random();
+            //int[] arr = new int[n];
+            //Console.WriteLine();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = random2.Next(1, 5);
+            //    Console.WriteLine(arr[i]);
+            //}
+            //Console.WriteLine();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < 3 && arr[i] > 0)
+            //    {
+            //        count++;
+
+            //    }
+
+            //}
+            //for (int i = 0; i < 1; i++)
+            //{
+            //    if (count > 0)
+            //    {
+            //        Console.WriteLine("кол-во плохих оценок: " +  count);
+            //    }
+            //}
 
         }
     }
