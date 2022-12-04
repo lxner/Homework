9d19ca0439474f45744eded2887daa467b4db38c
Lev <levasik228@mail.ru>
2022-12-05 01:46:52 +0500

    T_8_16

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Domashka;
 using Domashka.T_8;
 
-T_8_15 prog = new T_8_15();
+T_8_16 prog = new T_8_16();
 prog.Go();
---
+++ Domashka/T_8/T_8_16.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_8_16
+    {
+        public void Go()
+        {
+            int mi = 0;
+            Random random = new Random();
+            int max = -1000;
+            int sum = 0;
+            int max3 = 0;
+            int mj = 0;
+            Console.WriteLine("Введите кол-во работников: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = 3; //kvartal
+            int[,] ar = new int[a,b];
+            Console.WriteLine();
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i+1 + " работник");
+                for (int j = 0; j < b; j++)
+                {
+
+                    ar[i,j] = random.Next(0,100);
+                    Console.WriteLine(j+1 + " квартал: " + ar[i,j]);
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    sum = sum + ar[i, j]; // sum
+                    if (ar[i,j] > max)
+                    {
+
+                        max = ar[i,j];
+                        mi = i;
+                        mj = j;
+                    }
+                }
+            }
+            Console.WriteLine("максимальная зп в таблице: " + max);
+            Console.WriteLine("Порядковый номер работника получившего за квартал наибольшую сумму: " + mi);
+            Console.WriteLine("максимальная общая зарплата работников была в " + mj + " месяце");
+        }
+    }
+}
+// в каком месяце общая зарплата всех работников была максимальной
