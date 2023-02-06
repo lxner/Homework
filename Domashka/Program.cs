553d6a12d9b03433820948613c97d4b793072844
Lev <levisaev848@gmail.com>
2023-02-06 23:32:30 +0500

    T_12_8_B

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Domashka;
 using Domashka.exclusive;
+using Domashka.T_12;
 using Domashka.T_8;
 
-T_12_8 prog = new T_12_8();
+T_12_8_B prog = new T_12_8_B();
 prog.Go();
---
+++ Domashka/T_12/T_12_8_B.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Составить программу
+// b) заменяющую значение любого элемента двумерного массива на число b
+
+namespace Domashka.T_12
+{
+    internal class T_12_8_B
+    {
+        public void Go()
+        {
+            Random rand = new Random();
+            int[,] arr = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[i, j] = rand.Next(0, 10);
+                }
+            }
+            Console.WriteLine("исходный массив");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("результат: \n");
+            Console.WriteLine("Введите номер строки которую нужно заменить: ");
+            int l = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите номер столбца которого нужно заменить: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите само число: ");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[l, k] = x;
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
