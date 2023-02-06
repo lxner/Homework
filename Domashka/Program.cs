906888fea08c62985149a939da8f51b9c3679ea6
Lev <levisaev848@gmail.com>
2023-02-06 23:24:27 +0500

    T_12_8

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -2,5 +2,5 @@
 using Domashka.exclusive;
 using Domashka.T_8;
 
-T_12_110 prog = new T_12_110();
+T_12_8 prog = new T_12_8();
 prog.Go();
---
+++ Domashka/T_12/T_12_8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Составить программу
+// а) заменяющую значение любого элемента второго столбца двумерного массива на число 13
+namespace Domashka
+{
+    internal class T_12_8
+    {
+        public void Go()
+        {
+            Random rand = new Random();
+            int[,] arr = new int[5,5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[i, j] = rand.Next(0,10);
+                }
+            }
+            Console.WriteLine("исходный массив");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(arr[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("результат: \n");
+            Console.WriteLine("Введите номер строки которую нужно заменить: ");
+            int l = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[l, 2] = 13;
+                    Console.Write(arr[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
