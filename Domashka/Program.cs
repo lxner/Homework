6abcd61fe301234b2843b22c72a5705f7c7c4842
Lev <levisaev848@gmail.com>
2023-02-07 20:10:52 +0500

    Array

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -3,5 +3,5 @@ using Domashka.exclusive;
 using Domashka.T_12;
 using Domashka.T_8;
 
-T_12_9_A prog = new T_12_9_A();
+Domashka.Array prog = new Domashka.Array();
 prog.Go();
---
+++ Domashka/exclusive/Array.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Описать класс одномерный массив, содержащий его элементы и их количество, а
+// также методы: вывода всех значений на экран, нахождения минимального элемента,
+// нахождения максимального элемента.
+namespace Domashka
+{
+    class MyArr
+    {
+        int[] arr;
+        public int Length;
+
+        public MyArr(int Size)
+        {
+            arr = new int[Size];
+            Length = Size;
+        }
+        public int this[int index]
+        {
+            set
+            {
+                arr[index] = value;
+            }
+
+            get
+            {
+                return arr[index];
+            }
+        }
+    }
+    internal class Array
+    {
+        public void Go()
+        {
+            Random random = new Random();
+            var arr1 = new MyArr(5);
+            int count = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = random.Next(0,100);
+                Console.Write(arr1[i] + " ");
+                count++;
+            }
+            Console.WriteLine("\nКол-во элементов в массиве: " + count + "\n");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (min > arr1[i])
+                {
+                    min = arr1[i];
+                }
+                if (max < arr1[i])
+                {
+                    max = arr1[i];
+                }
+            }
+            Console.WriteLine("minimum: " + min);
+            Console.WriteLine("maximum: " + max);
+        }
+    }
+}
