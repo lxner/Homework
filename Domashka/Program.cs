8b68054b288835c02beba660d9cd8f6f57a97b1b
Lev <levisaev848@gmail.com>
2023-03-15 01:28:14 +0500

    T_12_10

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -1,7 +1,6 @@
-﻿using Domashka;
-using Domashka.exclusive;
+﻿using Domashka.exclusive;
 using Domashka.T_12;
 using Domashka.T_8;
 
-T_12_9_B prog = new T_12_9_B();
+T_12_10 prog = new T_12_10();
 prog.Go();
