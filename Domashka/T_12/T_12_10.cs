6f0031e084060bad946a0c8547615d1a37f7bd54
Lev <levisaev848@gmail.com>
2023-03-24 02:03:05 +0500

    T_12_10

--- Domashka/T_12/T_12_10.cs
+++ Domashka/T_12/T_12_10.cs
@@ -36,38 +36,27 @@ namespace Domashka.T_12
             int temp = 0;
             int tempp = 0;
             Console.WriteLine("Введите откуда взять элемент из столбца: ");
-            int z = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите откуда взять элемент со строки: ");
-            int v = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите в какой столбец вставить: ");
-            int c = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите в какую строку вставить: ");
             int x = int.Parse(Console.ReadLine());
-            int[] stringh = new int[5];
-            int[] columnh = new int[5];
-
+            Console.WriteLine("Введите в какую строку вставить: ");
+            int y = int.Parse(Console.ReadLine());
+            temp = arr[a,b];
+            arr[a, b] = arr[x, y];
+            arr[x, y] = temp;
             Console.WriteLine();
+          //  шаг 4, выводим обработанный массив
             for (int i = 0; i < 5; i++)
             {
-                tempp = stringh[z];
-                stringh[z] = temp;
-             
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
             }
-            for (int j = 0; j < 5; j++)
-            {
-                temp = columnh[v];
-                columnh[v] = tempp;
-            }
-            // шаг 4, выводим обработанный массив
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    for (int j = 0; j < 5; j++)
-            //    {
-            //        Console.Write(arr[i,j] + "\t");
-            //    }
-            //    Console.WriteLine();
-            //}
-           
+
         }
     }
 }
@@ -77,71 +66,3 @@ namespace Domashka.T_12
 
 
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//// составить программу которая меняет два любых элемента
-//// двумернгого массива
-//namespace Domashka.T_12
-//{
-//    internal class T_12_10
-//    {
-//        public void Go()
-//        {
-//            // шаг 1, заполняем массив
-//            int[,] arr = new int[5, 5];
-//            Random rnd = new Random();
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    arr[i, j] = rnd.Next(0, 25);
-//                }
-//            }
-//            // шаг 2, выводим массив
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    Console.Write(arr[i, j] + " \t");
-//                }
-//                Console.WriteLine();
-//            }
-//            // шаг 3, работа с массивом, поменять местами
-//            Console.WriteLine("Обработанный массив\n");
-//            int temp = 0;
-//            int tempp = 0;
-//            Console.WriteLine("Введите откуда взять элемент из столбца: ");
-//            int z = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Введите откуда взять элемент со строки: ");
-//            int v = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Введите в какой столбец вставить: ");
-//            int c = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Введите в какую строку вставить: ");
-//            int x = int.Parse(Console.ReadLine());
-//            Console.WriteLine();
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    temp = arr[z, v];
-//                    tempp = arr[c, x];
-//                    arr[c, x] = temp;
-//                    arr[z, v] = tempp;
-//                }
-
-//            }
-//            // шаг 4, выводим обработанный массив
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    Console.Write(arr[i, j] + "\t");
-//                }
-//                Console.WriteLine();
-//            }
-//        }
-//    }
-//}
