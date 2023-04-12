57ec5bbfb752d9d00bcd6bfafbeee2c7b41f2ed7
Lev <levisaev848@gmail.com>
2023-04-13 01:36:50 +0500

    T_12_42

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -3,5 +3,5 @@ using Domashka.exclusive;
 using Domashka.T_12;
 using Domashka.T_8;
 
-T_12_41 prog = new T_12_41();
+T_12_42 prog = new T_12_42();
 prog.Go();
---
+++ Domashka/T_12_42.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    internal class T_12_42
+    {
+        public void Go()
+        {
+            Random rnd = new Random();
+            int vagon = 18;
+            int mesta = 36;
+            int[,] arr = new int[vagon, mesta];
+            for (int i = 0; i < vagon; i++)
+            {
+                for (int j = 0; j < mesta; j++)
+                {
+                    arr[i, j] = rnd.Next(0, 2);
+                }
+            }
+            for (int i = 0; i < vagon; i++)
+            {
+                for (int j = 0; j < mesta; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int count = 0;
+            Console.WriteLine("В каком вагоне посчитать кол-во свободных билетов? ");
+            int a = int.Parse(Console.ReadLine());
+            for (int j = 0; j < mesta; j++)
+            {
+                if (arr[a, j] == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("число свободных мест = " + count);
+        }
+    }
+}
