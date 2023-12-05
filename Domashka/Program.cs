a03010d680e8adb1711218e959bd3a2d6f868bf9
Lev <levisaev848@gmail.com>
2023-12-06 04:22:55 +0500

    Insert_Sort

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -4,5 +4,5 @@ using Domashka.sort;
 using Domashka.T_12;
 using Domashka.T_8;
 
-ChooseSort prog = new ChooseSort();
+Insert prog = new Insert();
 prog.Go();
---
+++ Domashka/sort/Insert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka.sort
+{
+    internal class Insert
+    {
+        public void Go()
+        {
+            int index;
+            int number;
+            int[] mra = { 8, 2, 5, 4, 1, 9 };
+            for (int i = 0; i < mra.Length; i++)
+            {
+                Console.Write(mra[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mra.Length; i++)
+            {
+                index = i;
+                number = mra[i];
+                while (index > 0 && number < mra[index-1])
+                {
+                    mra[index] = mra[index - 1];
+                    index--;
+                }
+                mra[index] = number;
+            }
+            for (int i = 0; i < mra.Length; i++)
+            {
+                Console.Write(mra[i] + " ");
+            }
+        }
+    }
+}
